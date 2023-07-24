98e5faf9be57d93f69a45c5291d60d2c09b7ed4e
breno <breno.ms1997@gmail.com>
2023-07-24 17:30:48 -0300

    adds threaded requests version

--- main.py
+++ main.py
@@ -5,31 +5,49 @@ from os import listdir, wait
 from os.path import isfile, join
 import re
 import string
+import threading
 from googleapiclient import discovery
 import json
+from retrying import retry
 import logging
 
 class PerspectiveRequests():
-    def __init__(self, dataframe_path, text_field):
+    def __init__(self, dataframe_path, text_field, api_key_path ,n_threads=10):
         """
         dataframe_path: path to the dataframe used
         text_field: field within the dataframe to be analyzed by perspective
-
+        n_threads: number of threads
         """
         self._path = dataframe_path
         self._text_field = text_field
-        self.df = pd.read_csv(self._path,  dtype = 'str')
-        print('Number of rows:', len(self.df))
-
-
-    # def _get_list_of_files(self):
-    #     # adds path to file name
-    #     self._files_list = [
-    #         self._path + "/" + f for f in listdir(self._path) if isfile(join(self._path, f))]
-
-    def create_client(self, api_key_path):
+        self.n_threads = n_threads
+        self.api_key_path = api_key_path
         self.api_key = open(api_key_path, "r").read()
 
+        self.df = pd.read_csv(self._path,lineterminator='\n')
+        self.df = self.df.sample(100)
+        print('Number of rows:', len(self.df))
+        self.df.dropna(subset = [text_field], inplace=True)
+        print("Number of rows after dropping NaN rows:", len(self.df))
+
+    def threads_create_client(self):
+        """
+        Used to create multiple client objects for threaded use
+        """
+        client = discovery.build(
+            "commentanalyzer",
+            "v1alpha1",
+            developerKey=self.api_key,
+            discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+            static_discovery=False,
+        )
+        return client
+
+    def global_create_client(self):
+        """
+        creates one client object for the entire class. Do not use this if you are using threads
+
+        """
         self.client = discovery.build(
             "commentanalyzer",
             "v1alpha1",
@@ -38,14 +56,43 @@ class PerspectiveRequests():
             static_discovery=False,
         )
 
-    def toxicity_request(self, text, sleep_time=0.1):
+    def _slice_list(self, list_to_slice, num_sub_lists):
+        """
+        list_to_slice: list to slice
+        num_sub_lists: number of lists after the slice
+
+        returns a list of lists
+        """
+        sub_lists = [
+            list_to_slice[i * (len(list_to_slice) // num_sub_lists):(i + 1) * (len(list_to_slice) // num_sub_lists)]
+            for i in range(num_sub_lists)
+        ]
+        return sub_lists
+    
+    def _loop_requests(self, text_list, thread_id, client,sleep_time=0.4):
+        """
+        helper to loop requests over each list of text
+        """
+        for text in text_list:
+            self.toxicity_request(text, thread_id, client, sleep_time=sleep_time)
+
+
+    @retry(wait_exponential_multiplier=1000, wait_exponential_max=20000, stop_max_attempt_number=10)
+    def toxicity_request(self, text, thread_id, client,sleep_time=0.4):
+        """
+        text: text to be submitted to Perspective
+        thread_id: thread id for debugging/logging purposes
+        client: each thread must have a different client
+        """
+        text_id = text[1]
+        text = text[0]
         time.sleep(sleep_time)
         attributes = {'TOXICITY': -1,
-                      'SEVERE_TOXICITY': -1,
-                      'IDENTITY_ATTACK': -1,
-                      'INSULT': -1,
-                      'PROFANITY': -1,
-                      'THREAT': -1}
+                        'SEVERE_TOXICITY': -1,
+                        'IDENTITY_ATTACK': -1,
+                        'INSULT': -1,
+                        'PROFANITY': -1,
+                        'THREAT': -1}
 
         analyze_request = {
             'comment': {'text': text},
@@ -60,126 +107,42 @@ class PerspectiveRequests():
         }
 
         try:
-            response = self.client.comments().analyze(body=analyze_request).execute()
+            response = client.comments().analyze(body=analyze_request).execute()
             attributes["TOXICITY"] = response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
             attributes["SEVERE_TOXICITY"] = response["attributeScores"]["SEVERE_TOXICITY"]["summaryScore"]["value"]
             attributes["IDENTITY_ATTACK"] = response["attributeScores"]["IDENTITY_ATTACK"]["summaryScore"]["value"]
             attributes["INSULT"] = response["attributeScores"]["INSULT"]["summaryScore"]["value"]
             attributes["PROFANITY"] = response["attributeScores"]["PROFANITY"]["summaryScore"]["value"]
             attributes["THREAT"] = response["attributeScores"]["THREAT"]["summaryScore"]["value"]
+            print("Thread",thread_id,":",attributes["TOXICITY"], "text_id-correct:",text_id)
+            return attributes
         except Exception as e:
-            print(e)
+            print("text_id:",text_id,e)
+            raise IOError("text_id:",text_id,e)
 
-        print(attributes["TOXICITY"])
-        return attributes
+    def threaded_requests(self):
+        """
+        performs threaded requests. Creates the specified amount of threads and speeds up the requests.
 
-    # def _returns_only_text(self, char):
-    #     return char in string.punctuation or char == " " or str.isalnum(char)
+        """
+        texts_list = []
+        for index, row in self.df.iterrows():
+            texts_list.append((row["comment_text"], row["comment_id"]))
 
-    # def _filters_string(self, string):
-    #     if(string):
-    #         return ''.join(list(filter(self._returns_only_text, string)))
-    #     else:
-    #         return " "
+        sliced_texts_list = self._slice_list(texts_list,self.n_threads)
+        threads = []
+        for thread_count in range(self.n_threads):
+            # one client for each thread. Otherwise we'll get errors
+            client = self.threads_create_client()
+            thread = threading.Thread(target=self._loop_requests, args=(sliced_texts_list[thread_count],thread_count,client))
+            thread.start()
+            threads.append(thread)
 
-    # def preprocess_text(self, overwrite=False, save_log=True, clean_title=False, clean_description=False):
-    #     if (save_log):
-    #         logging.basicConfig(level=logging.INFO, filename="preprocess.log", filemode="w",
-    #                             format='%(asctime)s,%(message)s', datefmt='%d-%b-%y %H:%M:%S')
-
-    #     if not hasattr(self, '_files_list'):
-    #         self._get_list_of_files()
-    #     for i in range(len(self._files_list)):
-    #         print("Processed "+"{:.4}".format(str(i /
-    #                                               len(self._files_list)*100))+"% of files")
-    #         df = pd.read_csv(self._files_list[i])
-    #         df = df.fillna(' ')
-
-    #         # Removes links from description
-    #         df['description'] = df['description'].str.replace(
-    #             'http\S+|www.\S+', '', case=False)
-
-    #         df['description'] = df['description'].apply(
-    #             lambda x: self._filters_string(x))
-
-    #         # Optional preprocessing
-    #         if(clean_title):
-    #             df['title'] = df['title'].apply(lambda x: "".join(
-    #                 [i for i in x if i not in string.punctuation]))
-    #             df['title'] = df['title'].apply(lambda x: x.lower())
-    #             df['title'] = df['title'].apply(lambda x: ' '.join(
-    #                 [word for word in x.split() if word not in (stop)]))
-    #         # Optional preprocessing
-    #         if(clean_description):
-    #             df['description'] = df['description'].apply(
-    #                 lambda x: x.lower())
-    #             df['description'] = df['description'].apply(lambda x: ' '.join(
-    #                 [word for word in x.split() if word not in (stop)]))
-    #             # punctuation needs to be last, or else breaks the urls
-    #             df['description'] = df['description'].apply(
-    #                 lambda x: "".join([i for i in x if i not in string.punctuation]))
-
-    #         if(overwrite):
-    #             df.to_csv(self._files_list[i], index=False)
-    #         else:
-    #             df.to_csv(self._files_list[i]+"_clean.csv", index=False)
-
-    #         if(save_log):
-    #             logging.info("Overwrite="+str(overwrite) +
-    #                          ",file:"+str(self._files_list[i]))
-
-    def retrieve_features(self, save_log=True):
-        if(save_log):
-            logging.basicConfig(level=logging.INFO, filename="features.log", filemode="w",
-                                format='%(asctime)s,%(message)s', datefmt='%d-%b-%y %H:%M:%S')
-        try:
-            aux_column = self.df.apply(lambda row: pp.toxicity_request(
-                str(row[self._text_field])), axis=1)
-
-            self.df["toxicity"] = aux_column.apply(
-                lambda x: x["TOXICITY"])
-            self.df["severe_toxicity"] = aux_column.apply(
-                lambda x: x["SEVERE_TOXICITY"])
-            self.df["insult"] = aux_column.apply(
-                lambda x: x["INSULT"])
-            self.df["profanity"] = aux_column.apply(
-                lambda x: x["PROFANITY"])
-            self.df["threat"] = aux_column.apply(
-                lambda x: x["THREAT"])
-            self.df["identity_attack"] = aux_column.apply(
-                lambda x: x["IDENTITY_ATTACK"])
-    # ADICIONAR ATAQUE DE IDENTIDADE
-        except Exception as e:
-            print(e)
-            self.df["toxicity"] = -1
-            self.df["severe_toxicity"] = -1
-            self.df["insult"] = -1
-            self.df["profanity"] = -1
-            self.df["threat"] = -1
-            self.df["identity_attack"] = -1
-            if(save_log):
-                logging.error("ERROR,"+str(e))
+        for thread in threads:
+            thread.join()
 
 
-pp = PerspectiveRequests("comments_sample.csv","comment_text")
+pp = PerspectiveRequests("/data/Downloads/comments.csv","comment_text", "api_key", n_threads=10)
 # pp.preprocess_text(overwrite=True)
-pp.create_client("api_key")
-pp.retrieve_features()
-
-# print(pp.toxicity_request("eu sou o breno"))
-# time.sleep(1)
-# pp.toxicity_request("what the fuck!!!")
-# time.sleep(1)
-# pp.toxicity_request("WHAT THE FUCK!!!")
-# time.sleep(1)
-# pp.toxicity_request("what the fuck...")
-# time.sleep(1)
-# print("Sem stop words\n")
-# pp.toxicity_request("what fuck")
-# time.sleep(1)
-# pp.toxicity_request("what fuck!!!")
-# time.sleep(1)
-# pp.toxicity_request("WHAT FUCK!!!")
-# time.sleep(1)
-# pp.toxicity_request("what fuck...")
-
+# pp.retrieve_features()
+pp.threaded_requests()
